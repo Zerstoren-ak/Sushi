cfcb992261a70907abf264be503131e9d44cb7a9
Andrey <firebird_07@mail.ru>
2020-05-04 01:50:47 +0300

    Popup added

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,23 @@
+
+"use strict";
+//SLider
 $('.carousel').carousel({
     interval: false,
 })
+
+
+//Popups
+const btnFormLogin = document.getElementById('btnLoginForm');
+const btnFormClose = document.getElementById('btnFormClose');
+const formLogin = document.getElementById('loginForm');
+
+
+btnFormLogin.onclick = function () {
+    document.body.classList.add('no-scroll');
+    formLogin.classList.add('show');
+};
+
+btnFormClose.onclick = function () {
+    document.body.classList.remove('no-scroll');
+    formLogin.classList.remove('show');
+};
